aa2474f59ec0dcfce6441bcf438e2a053fedebe6
ElsaMach <elsa.machet@ipilyon.net>
2019-03-15 14:27:20 +0100

    v.1.3.1

---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.ipiecoles.java.java350.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.asserts.Assertion;
+
+@SpringBootTest
+public class EmployeRepositoryTest {
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    @Test
+    public void testFindLastMatriculeEmpty(){
+        //Given
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertThat(lastMatricule).isNull();
+    }
+}
+
